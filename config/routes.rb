502354f23b9298e523a4adb51a9bc665f66d2875
Jose Vega <jocvegar@gmail.com>
2022-11-28 08:49:16 -0600

    add LineItem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'line_items/new'
+  get 'line_items/create'
+  get 'line_items/edit'
+  get 'line_items/update'
+  get 'line_items/destroy'
   resources :line_item_dates
   root 'pages#home'
   devise_for :users
   resources :quotes
   resources :quotes do
-    resources :line_item_dates, except: [:index, :show]
+    resources :line_item_dates, except: [:index, :show] do
+      resources :line_items, except: [:index, :show]
+    end
   end
 end
