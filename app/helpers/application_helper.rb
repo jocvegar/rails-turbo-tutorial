b12eb7f6f0a081c9bfb07bff4c9d9a47588b5a83
Jose Vega <jocvegar@gmail.com>
2022-12-02 07:20:38 -0600

    little playing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,4 @@
 module ApplicationHelper
-  # def flash_messages
-	# 	flash.each do |msg_type, message|
-	# 		concat(
-	# 			content_tag(:div, id: "flash-wrapper", class: "container-fluid", style: "padding-top:0.5rem;") do
-	# 				concat(
-	# 					content_tag(:div, message) do
-	# 						concat content_tag(:span, message.html_safe, class: 'message')
-	# 					end
-	# 				)
-	# 			end
-	# 		)
-	# 	end
-	# 	nil
-	# end
 	def render_turbo_stream_flash_messages
     turbo_stream.prepend "flash", partial: "layouts/flash"
   end
@@ -21,6 +7,14 @@ module ApplicationHelper
     if object.errors.any?
       tag.div class: "error-message" do
         object.errors.full_messages.to_sentence.capitalize
+      end
+    end
+  end
+
+	def field_form_error_notification(object, field)
+    if object.errors.include?(field.to_sym)
+      tag.div class: "error-message-small" do
+        object.errors.full_messages_for(field.to_sym).first
       end
     end
   end
